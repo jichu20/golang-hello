79f2be94ac4d9af5191c481b79a5de27cd823fc1
borjajavier.sanchez <borjajavier.sanchez@bbva.com>
2017-12-20 15:29:29 +0100

    add hello World

---
+++ router.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+
+	server := http.ListenAndServe(":8080", router)
+
+	log.Fatal(server)
+
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
+}
